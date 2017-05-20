59b74f10d864ca0bd152d109735975601b2a65f4
christopherpeaden <peadenchristopher@gmail.com>
2017-05-19 23:13:04 -0400

    Add event listeners to new list items

--- assets/javascripts/delete-buttons.js
+++ assets/javascripts/delete-buttons.js
@@ -1,23 +1,24 @@
-function addEventsToDeleteButtons() {
-    var deleteButtons = document.getElementsByClassName("delete-button");
-    for (var x = 0; x < deleteButtons.length; x++) {
-        var deleteButton = deleteButtons[x];
+var deleteButtons = document.getElementsByClassName("delete-button");
 
-        deleteButton.addEventListener("click", function() {
-            var id = this.getAttribute('data-id');
-            var itemToDelete = document.getElementById('list-item-' + id);
-
-            var obj = new XMLHttpRequest();
-            obj.open("DELETE", "http://localhost?id=" + id);
-            obj.send();
-            obj.onreadystatechange = function() {
-                if(obj.readyState == XMLHttpRequest.DONE && obj.status == 200) {
-                    var list = document.getElementsByTagName('ul')[0]; 
-                    list.removeChild(itemToDelete);
-                }
-            }
-        })
-    }
+for (var x = 0; x < deleteButtons.length; x++) {
+    addEventsToDeleteButton(deleteButtons[x].dataset.id);
 }
 
-addEventsToDeleteButtons();
+function addEventsToDeleteButton(id) {
+    var deleteButton = document.querySelectorAll("[data-id='" + id + "']")[1];
+
+    deleteButton.addEventListener("click", function() {
+        var id = this.getAttribute('data-id');
+        var itemToDelete = document.getElementById('list-item-' + id);
+
+        var obj = new XMLHttpRequest();
+        obj.open("DELETE", "http://localhost?id=" + id);
+        obj.send();
+        obj.onreadystatechange = function() {
+            if(obj.readyState == XMLHttpRequest.DONE && obj.status == 200) {
+                var list = document.getElementsByTagName('ul')[0]; 
+                list.removeChild(itemToDelete);
+            }
+        }
+    })
+}
--- assets/javascripts/edit-buttons.js
+++ assets/javascripts/edit-buttons.js
@@ -1,54 +1,60 @@
-function addEventsToEditButtons() {
-    var editButtons = document.getElementsByClassName("edit-button");
-    for (var x = 0; x < editButtons.length; x++) {
-        var editButton = editButtons[x];
+var editButtons = document.getElementsByClassName("edit-button");
 
-        editButton.addEventListener("click", function() {
-            var id = this.getAttribute('data-id');
-            var form = document.createElement('form');
-            var input = document.createElement('input');
+for (var x = 0; x < editButtons.length; x++) {
+    addEventsToEditButton(editButtons[x].dataset.id);
+}
 
-            var list = document.getElementById('content-list');
-            var id = this.getAttribute('data-id');
-            var listItem = document.getElementById('list-item-' + id);
-            var listItemText = document.getElementById('list-item-' + id).firstChild;
-            var text = listItemText.wholeText;
-            input.value = text;
-            form.appendChild(input);
-            var listEditButton = document.getElementById('list-item-' + id).children[0];
-            var listDeleteButton = document.getElementById('list-item-' + id).children[1];
+function addEventsToEditButton(id) {
+    var editButton = document.querySelectorAll("[data-id='" + id + "']")[0];
 
-            listItem.replaceChild(form, listItemText);
-            listItem.removeChild(listEditButton);
-            listItem.removeChild(listDeleteButton);
-            var saveLink = document.createElement('a');
-            saveLink.href = "#";
-            saveLink.setAttribute("data-id", id);
-            var saveText = document.createTextNode('Save');
-            saveLink.appendChild(saveText);
+    editButton.addEventListener("click", function() {
+        var id = this.getAttribute('data-id');
+        var form = document.createElement('form');
+        var input = document.createElement('input');
 
-            var cancelLink = document.createElement('a');
-            cancelLink.href = "#";
-            var cancelText = document.createTextNode('Cancel');
-            cancelLink.appendChild(cancelText);
+        var list = document.getElementById('content-list');
+        var id = this.getAttribute('data-id');
+        var listItem = document.getElementById('list-item-' + id);
+        var listItemText = document.getElementById('list-item-' + id).firstChild;
+        var text = listItemText.wholeText;
+        input.value = text;
+        form.appendChild(input);
+        var listEditButton = document.getElementById('list-item-' + id).children[0];
+        var listDeleteButton = document.getElementById('list-item-' + id).children[1];
 
-            listItem.appendChild(saveLink);
-            listItem.appendChild(cancelLink);
+        listItem.replaceChild(form, listItemText);
+        listItem.removeChild(listEditButton);
+        listItem.removeChild(listDeleteButton);
+        var saveLink = document.createElement('a');
+        saveLink.href = "#";
+        saveLink.setAttribute("data-id", id);
+        var saveText = document.createTextNode('Save');
+        saveLink.appendChild(saveText);
 
-            saveLink.addEventListener("click", function() {
-                var obj = new XMLHttpRequest();
-                sendSaveRequest(obj, saveLink, input);
-                obj.onreadystatechange = function() {
-                    if(obj.readyState == XMLHttpRequest.DONE && obj.status == 200) {
-                        parser = new DOMParser();
-                        doc = parser.parseFromString(obj.response, "text/html");
-                        var list = document.getElementsByTagName('ul')[0]; 
-                        list.replaceChild(doc.firstChild.children[1].firstChild, listItem);
-                    }
+        var cancelLink = document.createElement('a');
+        cancelLink.href = "#";
+        var cancelText = document.createTextNode('Cancel');
+        cancelLink.appendChild(cancelText);
+
+        listItem.appendChild(saveLink);
+        listItem.appendChild(cancelLink);
+
+        saveLink.addEventListener("click", function() {
+            var obj = new XMLHttpRequest();
+            sendSaveRequest(obj, saveLink, input);
+            obj.onreadystatechange = function() {
+                if(obj.readyState == XMLHttpRequest.DONE && obj.status == 200) {
+                    parser = new DOMParser();
+                    doc = parser.parseFromString(obj.response, "text/html");
+                    var newListItem = doc.firstChild.children[1].firstChild;
+                    var list = document.getElementsByTagName('ul')[0]; 
+                    list.replaceChild(newListItem, listItem);
+                    addEventsToEditButton(newListItem.children[0].dataset.id);
+                    addEventsToDeleteButton(newListItem.children[1].dataset.id);
                 }
-            });
-        })
-    }
+            }
+        });
+    })
 }
 
 function sendSaveRequest(obj, saveLink, input) {
@@ -57,4 +63,3 @@ function sendSaveRequest(obj, saveLink, input) {
     obj.open("PUT", "http://localhost?id=" + saveId + "&title=" + title);
     obj.send();
 }
-addEventsToEditButtons();

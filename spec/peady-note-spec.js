cbf17c50e98c7979ef4b0ecc874c35a3e1e4cef4
christopherpeaden <peadenchristopher@gmail.com>
2017-05-26 00:06:26 -0400

    Add selenium integration specs for Note CRUD actions

--- spec/peady-note-spec.js
+++ spec/peady-note-spec.js
@@ -7,33 +7,61 @@ var path = require('chromedriver').path;
 var service = new chrome.ServiceBuilder(path).build();
 chrome.setDefaultService(service);
 
-describe('Peady Note', function() {
+describe('Note', function() {
     beforeEach(function(done) {
-        this.webdriver = new webdriver.Builder().
+        driver = new webdriver.Builder().
             withCapabilities(webdriver.Capabilities.chrome()).
             build();
 
-        this.webdriver.get('http://localhost').then(done);
+        driver.get('http://localhost').then(done);
     });
 
-    // Close the website after each test is run (so that it is opened fresh each time)
-    /*
+    // Close the website after each test is run (so that it is opened new each time)
     afterEach(function(done) {
-        this.webdriver.quit().then(done);
-    });
-    */
-
-    xit('Responds to GET request', function() {
-        var res = request('http://localhost', function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-	    }
-
-	})
-	expect(res).toEqual(res);
+        setTimeout(function() {
+            driver.quit().then(done);
+        }, 2500);
     });
 
-    it("Integration", function() {
-        this.webdriver.findElement(webdriver.By.name('title')).sendKeys('new item');
-        this.webdriver.findElement(webdriver.By.name('submit')).click();
+    it("responds to CRUD actions", function() {
+        // Create new notes
+        for (var x = 0; x < 3; x++) {
+            driver.findElement(webdriver.By.name('title')).sendKeys('Test Text ' + (x + 1));
+            driver.findElement(webdriver.By.name('submit')).click();
+            driver.findElement(webdriver.By.name('title')).clear();
+        }
+
+        // Click the edit buttons
+        driver.wait(webdriver.until.elementsLocated(webdriver.By.linkText('Edit')), 5 * 1000)
+            .then(edits => {
+                edits[0].click();
+                edits[1].click();
+                edits[2].click();
+
+                // Modify the text of the notes
+                driver.wait(webdriver.until.elementsLocated(webdriver.By.xpath('//input')), 5 * 1000)
+                    .then(inputs => {
+                        inputs[0].sendKeys(" updated");
+                        inputs[1].sendKeys(" edited");
+                        inputs[2].sendKeys(" changed");
+        
+                        // Click the save buttons
+                        driver.wait(webdriver.until.elementsLocated(webdriver.By.linkText('Save')), 5 * 1000)
+                            .then(saves => {
+                                saves[0].click();
+                                saves[1].click();
+                                saves[2].click();
+                                // Selenium will not wait for the delete buttons to appear before clicking them, so we are using a brief setTimout here
+                                setTimeout(() => {
+                                    driver.wait(webdriver.until.elementsLocated(webdriver.By.linkText('Delete')), 5 * 1000)
+                                        .then(dels => {
+                                            dels[0].click();
+                                            dels[1].click();
+                                            dels[2].click();
+                                        });
+                                }, 500);
+                            });
+                    });
+            });
     });
 });

aa07a70f8ed74233a8ae9945e915f720dd5e1bdd
christopherpeaden <peadenchristopher@gmail.com>
2017-06-06 00:01:38 -0400

    Refactor note specs into promises

--- spec/peady-note-spec.js
+++ spec/peady-note-spec.js
@@ -18,50 +18,34 @@ describe('Note', function() {
 
     // Close the website after each test is run (so that it is opened new each time)
     afterEach(function(done) {
-        setTimeout(function() {
-            driver.quit().then(done);
-        }, 2500);
+        driver.quit().then(done);
     });
 
     it("responds to CRUD actions", function() {
         // Create new notes
-        for (var x = 0; x < 3; x++) {
-            driver.findElement(webdriver.By.name('title')).sendKeys('Test Text ' + (x + 1));
-            driver.findElement(webdriver.By.name('submit')).click();
-            driver.findElement(webdriver.By.name('title')).clear();
+        for (var x = 1; x < 5; x++) {
+            driver.findElement(webdriver.By.name('title')).sendKeys("Test Text " + x)
+                .then(driver.findElement(webdriver.By.name('submit')).click())
+                .then(driver.findElement(webdriver.By.name('title')).clear());
         }
 
-        // Click the edit buttons
-        driver.wait(webdriver.until.elementsLocated(webdriver.By.linkText('Edit')), 5 * 1000)
-            .then(edits => {
-                edits[0].click();
-                edits[1].click();
-                edits[2].click();
+        for (var x = 1; x < 5; x++) {
+            // Click edit button
+            driver.wait(webdriver.until.elementLocated(webdriver.By.xpath("//ul/li[contains(., 'Test Text " + x + "')]/a[1]")), 5 * 1000)
+                .then(edit => edit.click())
 
-                // Modify the text of the notes
-                driver.wait(webdriver.until.elementsLocated(webdriver.By.xpath('//input')), 5 * 1000)
-                    .then(inputs => {
-                        inputs[0].sendKeys(" updated");
-                        inputs[1].sendKeys(" edited");
-                        inputs[2].sendKeys(" changed");
-        
-                        // Click the save buttons
-                        driver.wait(webdriver.until.elementsLocated(webdriver.By.linkText('Save')), 5 * 1000)
-                            .then(saves => {
-                                saves[0].click();
-                                saves[1].click();
-                                saves[2].click();
-                                // Selenium will not wait for the delete buttons to appear before clicking them, so we are using a brief setTimout here
-                                setTimeout(() => {
-                                    driver.wait(webdriver.until.elementsLocated(webdriver.By.linkText('Delete')), 5 * 1000)
-                                        .then(dels => {
-                                            dels[0].click();
-                                            dels[1].click();
-                                            dels[2].click();
-                                        });
-                                }, 500);
-                            });
-                    });
-            });
+            // Update note text
+            .then(driver.wait(webdriver.until.elementLocated(webdriver.By.xpath("//ul/li/form/input")), 5 * 1000)
+                .then(input => input.sendKeys("Updated")))
+
+            // Save the note 
+            .then(driver.wait(webdriver.until.elementLocated(webdriver.By.xpath("//ul/li/a[1]")), 5 * 1000)
+                .then(save => save.click()))
+
+            // Delete the note
+            .then(driver.wait(webdriver.until.elementLocated(webdriver.By.xpath("//li[contains(., 'Test Text " + x + " Updated')]/a[2]")), 5 * 1000)
+                .then(del => del.click()));
+        }
+
     });
 });
